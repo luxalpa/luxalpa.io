f82a90aa49a0f98c406c10be780e4b2a7a3c7ab1
Luxalpa <git@luxalpa.io>
2020-06-05 10:01:49 +0200

    Responsive version for mobile

--- src/components/views/project-overview.tsx
+++ src/components/views/project-overview.tsx
@@ -1,5 +1,5 @@
 import { Component, Vue } from "vue-property-decorator";
-import { stylesheet } from "typestyle";
+import { media, stylesheet } from "typestyle";
 import { Theme } from "@/theme";
 import { Projects, sortProjectsByDate } from "@/project-meta";
 import { Markdown } from "@/components/markdown";
@@ -10,21 +10,23 @@ const styles = stylesheet({
     margin: "0 -15px",
     display: "block",
     padding: "15px",
+    cursor: "pointer",
     $nest: {
       "&:hover, &:focus": {
         backgroundColor: Theme.hoverColor,
       },
     },
   },
-  name: {
-    color: "black",
-    font: 'bold 12pt "Roboto Slab"',
-    marginBottom: "10px",
-    display: "inline-block",
-  },
+  name: {},
   date: {
-    float: "right",
-    color: "#000",
+    color: "#5e5e5e",
+    fontWeight: "normal",
+  },
+  subheading: {
+    marginBottom: "10px",
+    display: "flex",
+    justifyContent: "space-between",
+    color: "black",
     font: 'bold 12pt "Roboto Slab"',
   },
   abstract: {
@@ -34,6 +36,15 @@ const styles = stylesheet({
   page: {
     width: "600px",
     margin: "35px auto",
+    ...media(
+      { minWidth: 0, maxWidth: 685 },
+      {
+        width: "100%",
+        paddingLeft: "35px",
+        paddingRight: "35px",
+        boxSizing: "border-box",
+      }
+    ),
   },
   topText: {
     margin: "0 0 15px",
@@ -59,7 +70,8 @@ export class ProjectOverview extends Vue {
         <div class={styles.topText}>
           <Markdown>
             I have been programming since 2003. The following is an incomplete
-            collection of my recent projects.
+            collection of my recent projects. Click on a project for more
+            information.
           </Markdown>
         </div>
         {projects.reverse().map((project) => (
@@ -68,9 +80,10 @@ export class ProjectOverview extends Vue {
             style={{ textDecoration: "none" }}
             class={styles.box}
           >
-            <span class={styles.name}>{project.name}</span>
-            <span class={styles.date}>{project.date}</span>
-            <br />
+            <div class={styles.subheading}>
+              <span class={styles.name}>{project.name}</span>
+              <span class={styles.date}>{project.date}</span>
+            </div>
             <span class={styles.abstract}>{project.abstract}</span>
           </router-link>
         ))}
--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,18 @@ import "typeface-roboto-slab";
 import "typeface-fira-sans";
 import "typeface-fira-code";
 import { cssRule } from "typestyle";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 Vue.config.productionTip = false;
+
+library.add(faChevronLeft, faChevronRight);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 cssRule("body", {
   font: "12pt/1.8 Roboto Slab",
